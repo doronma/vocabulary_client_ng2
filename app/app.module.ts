38e396dfbde5d1c418147b4f7c0dfc8ecce53f37
Doron Marcus <Doron Marcus>
2016-12-26 14:51:39 +0200

    initial dictionary version

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+
+import {HomeComponent } from './home/app.home';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ HomeComponent ],
+  bootstrap:    [ HomeComponent ]
 })
 export class AppModule { }
